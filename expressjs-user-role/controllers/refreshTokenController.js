f16def5ac3733ee6b6b0c1f46afa53a88e975d3c
Oguheartily <oguheartily@gmail.com>
2023-08-17 21:27:41 +0100

    console commented

--- expressjs-user-role/controllers/refreshTokenController.js
+++ expressjs-user-role/controllers/refreshTokenController.js
@@ -13,10 +13,10 @@ require('dotenv').config();
 
 const handleRefreshToken = (req, res) => {
     const cookies = req.cookies;
-    console.log(cookies);
+    // console.log(cookies);
     /**check if we have cookies, also optionally check if it has jwt properties */
     if(!cookies?.jwt) return res.sendStatus(401);
-    console.log(cookies.jwt);
+    // console.log(cookies.jwt);
     const refreshToken = cookies.jwt;
 
     const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
--- expressjs-user-role/controllers/registerController.js
+++ expressjs-user-role/controllers/registerController.js
@@ -33,7 +33,7 @@ const handleNewUser = async (req, res) => {
             path.join(__dirname, '..', 'model', 'users.json'),
             JSON.stringify(usersDB.users)
         );
-        console.log(usersDB.users);
+        // console.log(usersDB.users);
         res.status(201).json({ "success": `New user ${user} created successfully`});
     } catch (err) {
         res.status(500).json({ 'message': err.message });
